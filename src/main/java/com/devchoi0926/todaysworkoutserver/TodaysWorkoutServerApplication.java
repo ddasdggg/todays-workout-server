9bb19b6130521f771f8db84db35f56c033c31c14
choi0926 <ghtjd753753@gmail.com>
2022-06-15 03:22:39 +0900

    Initial commit

---
+++ src/main/java/com/devchoi0926/todaysworkoutserver/TodaysWorkoutServerApplication.java
@@ -0,0 +1,13 @@
+package com.devchoi0926.todaysworkoutserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysWorkoutServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodaysWorkoutServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devchoi0926/todaysworkoutserver/TodaysWorkoutServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devchoi0926.todaysworkoutserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaysWorkoutServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
